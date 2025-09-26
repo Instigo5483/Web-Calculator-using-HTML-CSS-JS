bdf45040952c143dd700cc5d35beb8dd86051c9a
Bastab Mushahary <87170926+Instigo5483@users.noreply.github.com>
2025-09-26 23:32:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let string = "";
+let button = document.querySelectorAll('.button');
+Array.from(button).forEach((button)=> {
+    button.addEventListener('click', (e)=> {
+        if(e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+
+        else if (e.target.innerHTML == 'C') {
+            string = '';
+            document.querySelector('input').value = string;
+        }
+
+        else if (e.target.innerHTML == 'backspace') {
+            string = string.substring(0, string.length - 1);
+            document.querySelector('input').value = string;
+        }
+
+        else if (e.target.innerHTML == 'e') {
+            string = string + Math.E;
+            document.querySelector('input').value = string;
+        }
+
+        else {
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
